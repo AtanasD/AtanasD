7082d828ca8431df3289d5d2033835b9aa05452d
AtanasD <atanasdinchev@gmail.com>
2021-04-09 18:16:59 +0300

    update links

--- skin.js
+++ skin.js
@@ -1,7 +1,7 @@
 // Garden Gnome Software - Skin
 // Pano2VR 6.0.2/17253
 // Filename: silhouette_v6.ggsk
-// Generated Fri Apr 9 17:59:08 2021
+// Generated Fri Apr 9 18:15:31 2021
 
 function pano2vrSkin(player,base) {
 	player.addVariable('opt_hotspot_preview', 2, true);
